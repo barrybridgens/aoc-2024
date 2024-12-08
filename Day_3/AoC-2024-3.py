4061e73c9a5df963a09c9f2e511b35f04309025f
Barry Bridgens <barry@bridgens.me.uk>
2024-12-08 18:21:45 +0000

    Day 2 problem 2 completed

--- Day_3/AoC-2024-3.py
+++ Day_3/AoC-2024-3.py
@@ -11,7 +11,7 @@ instructions = []
 with open("AoC-2024-3.dat") as f:
     data = f.read()
 
-    iter = re.finditer(r"mul\([0-9]+\,[0-9]+\)", data)
+    iter = re.finditer(r"(mul\([0-9]+\,[0-9]+\))|(do\(\))|(don\'t\(\))", data)
     indices = [m.span(0) for m in iter]
 
     print(len(indices))
@@ -19,17 +19,34 @@ with open("AoC-2024-3.dat") as f:
     for i in indices:
         instructions.append(data[i[0]:i[1]])
 
+    doing = True
+    process = True
+    
     for i in instructions:
-        print(i)
-        iter2 = re.finditer(r"[0-9]+", i)
-        indices2 = [m.span(0) for m in iter2]
 
-        n1 = int(i[int(indices2[0][0]): int(indices2[0][1])])
-        print(n1)
-        n2 = int(i[int(indices2[1][0]): int(indices2[1][1])])
-        print(n2)
+        if ("do()" in i):
+            doing = True
+            process = False
+        else:
+            if ("don\'t()" in i):
+                doing = False
+                process = False
+            else:
+                process = True
 
-        total = total + (n1 * n2)
+        if (doing):
+
+            if (process):
+                # print(i)
+                iter2 = re.finditer(r"[0-9]+", i)
+                indices2 = [m.span(0) for m in iter2]
+
+                n1 = int(i[int(indices2[0][0]): int(indices2[0][1])])
+                # print(n1)
+                n2 = int(i[int(indices2[1][0]): int(indices2[1][1])])
+                # print(n2)
+
+                total = total + (n1 * n2)
 
 print(total)
 
